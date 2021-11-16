b22a63c5bf762dbeb68d5c79bef3368436623740
gsombras <gsombras@alumnos.nebrija.es>
2021-11-16 21:28:35 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#Import json
+import persistencia_json
+
+#List creation
+
+marca = list()
+modelo = list()
+combustible = list()
+cilindrada = list()
+while True:
+    entrada = input("Marca (fin para terminar): ")
+    dato1 = input("Modelo: ")
+    dato2 = input("Combustible: ")
+    dato3 = input("Cilindrada: ")
+    if entrada != 'fin':
+        marca.append(entrada)
+        modelo.append(dato1)
+        combustible.append(dato2)
+        cilindrada.append(dato3)
+
+#Write file
+
+pj.store(lista.coches, "Coches.txt")
+
+#Clear list
+
+list.clear()
+
+
+
---
+++ persistencia_json.py
@@ -0,0 +1,8 @@
+#Store data
+import json
+
+
+def store(var, filename):
+
+def retrive(filename):
+    return json.loads(json.load(open(filename, 'r')))
