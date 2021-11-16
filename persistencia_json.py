0c3adf1a9acf0ff2167e2a00aa0f6f558a93bc18
gsombras <91568189+gsombras@users.noreply.github.com>
2021-11-16 21:58:56 +0100

    Update persistencia_json.py

--- persistencia_json.py
+++ persistencia_json.py
@@ -1,8 +1,8 @@
 #Store data
 import json
 
-
 def store(var, filename):
+    json.dump(json.dumps(list), open("Coches.txt", "w"))
 
 def retrive(filename):
     return json.loads(json.load(open(filename, 'r')))
